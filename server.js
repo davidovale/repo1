b90e49a78afdac3ef1df7759542a58a152462024
davidovale <dov21001@byui.edu>
2024-06-14 05:35:57 -0300

    partials created

--- server.js
+++ server.js
@@ -6,13 +6,18 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
 /* ***********************
- * Routes
+ * View Engine and Templates
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 /* ***********************
