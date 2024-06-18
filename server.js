7186d914834e50a01cc38cdc8a8f98acd330b24f
davidovale <dov21001@byui.edu>
2024-06-18 04:55:15 -0300

    last adjustments

--- server.js
+++ server.js
@@ -9,12 +9,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
-
-//Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+const static = require("./routes/static.js")
 
 
 /* ***********************
@@ -23,7 +18,15 @@ app.get("/", function(req, res){
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+
 app.use(static)
+
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
 
 /* ***********************
  * Local Server Information
