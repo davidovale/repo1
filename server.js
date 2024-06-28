c459865a307c0b2339ce615f6b7f2904bacae891
davidovale <dov21001@byui.edu>
2024-06-27 22:43:19 -0300

    server js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 //Index route
-//app.get("/", function(req, res){
-//  res.render("index", {title: "Home"})
-//})
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 app.get("/", baseController.buildHome)
 
