b35139d16cf1672b509d878bece9bc9e9906080c
davidovale <dov21001@byui.edu>
2024-07-05 01:26:49 -0300

    week 4 beginning

--- server.js
+++ server.js
@@ -14,7 +14,31 @@ const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute.js")
 const errorRoute = require("./routes/errorRoute")
 const utilities = require("./utilities/index")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
 
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -31,6 +55,9 @@ app.get("/", baseController.buildHome)
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Intentional Error route
+app.use('/account', utilities.handleErrors(accountRoute))
 
 // Intentional Error route
 app.use('/error', utilities.handleErrors(errorRoute))
