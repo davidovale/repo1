3e1783f139da27c39565fe3b93bed7e9386789d2
davidovale <dov21001@byui.edu>
2024-07-05 01:29:03 -0300

    week 4 beginning

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,6 +4,6 @@ const router = new express.Router()
 const accController = require("../controllers/accountController")
 const utilities = require("../utilities")
 
-router.get('/login', utilities.handleErrors(accountController.buildLogin));
+router.get('/login', utilities.handleErrors(accController.buildLogin));
 
 module.exports = router;
