2faab6c5c83ce51f85f9a3f106e4401e4dd9f5db
davidovale <dov21001@byui.edu>
2024-07-05 23:46:48 -0300

    test account files

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,4 +6,7 @@ const utilities = require("../utilities")
 
 router.get('/login', utilities.handleErrors(accountController.buildLogin));
 
+router.get('/register', utilities.handleErrors(accountController.buildRegister));
+
+
 module.exports = router;
